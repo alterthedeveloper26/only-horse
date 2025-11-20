05766dd9fb97c73fd906c614845aafd50b3244c2
Nguyen Van Bao Hoang (Kerry) <nvbhoang@cmcglobal.vn>
2025-11-20 09:44:13 +0700

    feat: add loggin user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import HomeScreen from "@/components/home/home-screen/HomeScreen";
 import AuthScreen from "@/components/home/auth-screen/AuthScreen";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export default function Home() {
-  const user = false;
+export default async function Home() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  console.log("THIS IS USER: ", user);
 
   return <main>{user ? <HomeScreen /> : <AuthScreen />}</main>;
 }
